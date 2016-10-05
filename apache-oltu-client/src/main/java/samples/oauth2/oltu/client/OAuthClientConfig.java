e988106762c462b993edba2cf8c4f69cb59739c6
Joe Grandja <jgrandja@pivotal.io>
2016-10-05 12:00:03 -0400

    Add Apache Oltu Client Sample

---
+++ apache-oltu-client/src/main/java/samples/oauth2/oltu/client/OAuthClientConfig.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package samples.oauth2.oltu.client;
+
+import java.util.Collection;
+
+/**
+ * @author Joe Grandja
+ */
+public class OAuthClientConfig {
+	private OAuthProvider provider;
+	private String authorizationUrl;
+	private String tokenUrl;
+	private String clientId;
+	private String clientSecret;
+	private Collection<String> scopes;
+
+	public OAuthClientConfig(OAuthProvider provider) {
+		setProvider(provider);
+	}
+
+	public OAuthProvider getProvider() {
+		return provider;
+	}
+
+	protected void setProvider(OAuthProvider provider) {
+		this.provider = provider;
+	}
+
+	public String getAuthorizationUrl() {
+		return authorizationUrl;
+	}
+
+	public void setAuthorizationUrl(String authorizationUrl) {
+		this.authorizationUrl = authorizationUrl;
+	}
+
+	public String getTokenUrl() {
+		return tokenUrl;
+	}
+
+	public void setTokenUrl(String tokenUrl) {
+		this.tokenUrl = tokenUrl;
+	}
+
+	public String getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+
+	public String getClientSecret() {
+		return clientSecret;
+	}
+
+	public void setClientSecret(String clientSecret) {
+		this.clientSecret = clientSecret;
+	}
+
+	public Collection<String> getScopes() {
+		return scopes;
+	}
+
+	public void setScopes(Collection<String> scopes) {
+		this.scopes = scopes;
+	}
+}
