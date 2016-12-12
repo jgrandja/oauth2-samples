dd3c4ad70952137b12dcfbecfd70a9296b932596
Joe Grandja <jgrandja@pivotal.io>
2016-12-12 13:21:44 -0500

    Remove AuthenticationManagerBuilder in SecurityConfig
    Fixes gh-5

--- apache-oltu-client/src/main/java/samples/oauth2/oltu/client/config/SecurityConfig.java
+++ apache-oltu-client/src/main/java/samples/oauth2/oltu/client/config/SecurityConfig.java
@@ -21,7 +21,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -81,13 +80,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 					.and()
 				.addFilterAfter(authorizationRequestRedirectFilter(), AbstractPreAuthenticatedProcessingFilter.class)
 				.addFilterAfter(authorizationCodeGrantProcessingFilter(), AuthorizationRequestRedirectFilter.class);
-	}
-	// @formatter:on
-
-	// @formatter:off
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(oauth2AuthenticationProvider());
 	}
 	// @formatter:on
 
